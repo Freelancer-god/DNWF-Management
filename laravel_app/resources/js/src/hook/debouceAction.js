82ab6554bfd19b9363d8f01305ae21bde1d79522
nguyen498 <67220820+nguyen498@users.noreply.github.com>
2024-05-31 15:40:06 +0700

    Commit

--- laravel_app/resources/js/src/hook/debouceAction.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-
-export const debouceAction = (func, data, time = 300, options = { leading: false, trailing: true }) => {
-    _.debounce(
-        func(data),
-        time,
-        options,
-    );
-};
--- laravel_app/resources/js/src/hook/throttlePressAcion.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-
-export const throttleOnPressAction = _.throttle(
-    (func, data) => {
-        func(data);
-    },
-    1000,
-    { trailing: false },
-);
